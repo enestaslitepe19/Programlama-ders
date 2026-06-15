df7d31b9f686b930ab9f96dad91fae7405e80ac4
enestaslitepe19 <enes.19.1905@gmail.com>
2026-06-15 10:30:15 +0300

    Proje

---
+++ PROJE23/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PROJE23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\proje22.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
